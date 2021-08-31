a9a2d0ccb032597d5327182f2ea93f546af74b15
Peter <oniicoder@gmail.com>
2021-08-31 01:12:43 +0000

    updated title

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Peter - Helix Sleep Challenge</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
